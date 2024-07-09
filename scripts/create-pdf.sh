066bfd21f95bcff56de75013f14274364755b88d
itsjohncs <johnsullivan.pem@gmail.com>
2024-07-09 11:21:03 -0700

    Add PDF pre-check.

--- scripts/create-pdf.sh
+++ scripts/create-pdf.sh
@@ -2,18 +2,48 @@
 
 set -ueo pipefail
 
+CHECK=false
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    --check)
+        CHECK=true
+        shift
+        ;;
+    *)
+        echo "USAGE: $0 [--check]" >&2
+        exit 1
+        ;;
+    esac
+done
+
 SCRIPT_DIR="$(
     cd "$(dirname "${BASH_SOURCE[0]}")"
     pwd -P
 )"
 
-if [[ $# -ne 0 ]]; then
-    echo "USAGE: $0" >&2
-    exit 1
-fi
+TEMP_PDF="$(mktemp /tmp/resume.XXXXXX.pdf)"
 
-set -x
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
-    --headless --disable-gpu --print-to-pdf="$SCRIPT_DIR/../resume.pdf" \
-    --no-pdf-header-footer ./index.htm
-echo "Done."
+    --headless --disable-gpu --print-to-pdf="$TEMP_PDF" \
+    --no-pdf-header-footer "$SCRIPT_DIR/../index.htm"
+
+if [[ $CHECK == true ]]; then
+    trap 'rm "$TEMP_PDF"' EXIT
+
+    RESUME_NUM_PAGES="$(identify -format '%n\n' "$SCRIPT_DIR/../resume.pdf" | head -n 1)"
+    TEMP_PAGES="$(identify -format '%n\n' "$TEMP_PDF" | head -n 1)"
+    if [[ $RESUME_NUM_PAGES -ne $TEMP_PAGES ]]; then
+        echo "PDFs have different page counts." >&2
+        exit 1
+    fi
+
+    for ((i = 0; i < TEMP_PAGES; ++i)); do
+        if ! compare "${SCRIPT_DIR}/../resume.pdf[$i]" "${TEMP_PDF}[$i]" /dev/null; then
+            echo "PDFs differ on page $i." >&2
+            exit 1
+        fi
+    done
+else
+    mv "$TEMP_PDF" "$SCRIPT_DIR/../resume.pdf"
+fi
